8acd8070f87acf448a0fb5c504a05235ec575a77
benjaminhxh <fooke@qq.com>
2018-03-12 15:07:12 +0800

    auth login logout api

--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond auth with a resource');
 });
 
+router.post('/login', function(req, res, next){
+  res.send('come from auth/login response');
+});
+
+router.get('/logout', function(req, res, next){
+  res.send('come from auth/logout response');
+});
 module.exports = router;
